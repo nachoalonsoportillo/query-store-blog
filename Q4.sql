52da07fa2b307647e8087e3524d5a681b52c5296
Nacho Alonso Portillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2026-04-05 22:43:41 +0200

    Create Q4.sql

---
+++ Q4.sql
@@ -0,0 +1,18 @@
+SELECT
+   o_orderpriority
+  ,COUNT(*)  AS ORDER_COUNT
+ FROM
+   ORDERS
+ WHERE  O_ORDERDATE  >= DATE '1996-04-01'
+ AND  O_ORDERDATE  < DATE '1996-04-01' + INTERVAL '3' MONTH
+ AND  EXISTS
+   (
+    SELECT   *
+    FROM  LINEITEM
+    WHERE  L_ORDERKEY = O_ORDERKEY
+    AND  L_COMMITDATE < L_RECEIPTDATE
+   )
+ GROUP BY
+   o_orderpriority
+ ORDER BY
+   o_orderpriority;
