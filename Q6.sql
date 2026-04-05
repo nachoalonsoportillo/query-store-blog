43454bedb9349c8382c76aefe56f6fae7db8692e
Nacho Alonso Portillo <41644064+nachoalonsoportillo@users.noreply.github.com>
2026-04-05 22:43:58 +0200

    Create Q6.sql

---
+++ Q6.sql
@@ -0,0 +1,9 @@
+SELECT
+   SUM(l_extendedprice*l_discount) AS REVENUE
+ FROM
+   LINEITEM
+ WHERE
+   L_SHIPDATE  >= DATE '1997-01-01'
+ AND  L_SHIPDATE  < DATE '1997-01-01' + INTERVAL '1' YEAR
+ AND  l_discount  BETWEEN '0.02' - 0.01 AND '0.02' + 0.01
+ AND  L_QUANTITY  < '25';
